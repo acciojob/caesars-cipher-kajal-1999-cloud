32271ad4925f25209123a510c13dd0f0a89fb26d
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-05-30 17:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; 
 	// check the availiblity
-	for(let i=0 ; i< inputString.length ; i++){
+	for(let i=0 ; i< encodedStr.length ; i++){
 		if(lookup[ encodedStr[i] ] === undefined){
 			decodeArr[i] += encodedStr[i] ;
 		}
